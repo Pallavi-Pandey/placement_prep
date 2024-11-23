e9da2316b9c47cafb0ffb6d8ed7577017c50e9f9
Pallavi-Pandey <pallavipandey181@gmail.com>
2024-11-23 14:33:25 +0530

    added day3

--- Day_3/a.py
+++ Day_3/a.py
@@ -1,3 +1,5 @@
+# Write a Python program to find the longest word in the sentence.
+
 sentence = "The quick brown fox jumps over the lazy dogggggggggg"
 words = sentence.split()
 print (max(words, key=len))
